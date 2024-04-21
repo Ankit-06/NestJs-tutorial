b8b30e6400c766a8b49e1a397b14bceb9968c568
Ankit <ankitkumar81458@gmail.com>
2024-04-21 18:01:42 +0530

    validation pipes in DTO

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsNotEmpty, IsEmail } from "class-validator";
+
+export class AuthDto {
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
