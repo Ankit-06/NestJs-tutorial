8e9c281d4652696d6737998f0b8be7b58e936521
Ankit <ankitkumar81458@gmail.com>
2024-04-21 13:58:06 +0530

    signup and signin routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
+
+    // auth/signup
+    @Post('signup')
+    signup() {
+        return this.authService.signup();
+    }
+
+    // auth/signin
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
 }
