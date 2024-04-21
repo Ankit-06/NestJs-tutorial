da5e7fc8c95a524a4716e87740daf473623d9040
Ankit <ankitkumar81458@gmail.com>
2024-04-21 18:04:07 +0530

    whitelist true for striping out extra properties

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true //stripout the fields which are not inside DTO
+  }));
   await app.listen(3333);
 }
 bootstrap();
